329de79ca4528251c47c22862610a52eceb5a9ab
Wangsx <wangsixxx@sina.com>
2017-12-07 10:12:02 +0800

    test

--- src/com/wsx/hellogit/hellogit.java
+++ src/com/wsx/hellogit/hellogit.java
@@ -4,5 +4,6 @@ public class hellogit {
 	public static void main(String[] args) {
 		System.out.println("welcome to use git~");
 		System.out.println("you can upload your code on github~");
+		System.out.println("some update test");
 	}
 }
