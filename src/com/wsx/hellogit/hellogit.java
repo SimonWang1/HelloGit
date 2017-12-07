c882bf0f2cd505b938eba8f03ee8d7d094b6c165
Wangsx <wangsixxx@sina.com>
2017-12-07 10:06:17 +0800

    test

---
+++ src/com/wsx/hellogit/hellogit.java
@@ -0,0 +1,8 @@
+package com.wsx.hellogit;
+
+public class hellogit {
+	public static void main(String[] args) {
+		System.out.println("welcome to use git~");
+		System.out.println("you can upload your code on github~");
+	}
+}
