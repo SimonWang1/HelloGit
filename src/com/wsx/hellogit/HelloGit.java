40f6e4f66c2e7d56da8a77472e5ab6bf27afef15
Wangsx <wangsixxx@sina.com>
2017-12-26 16:29:41 +0800

    update test

--- src/com/wsx/hellogit/HelloGit.java
+++ src/com/wsx/hellogit/HelloGit.java
@@ -2,7 +2,7 @@ package com.wsx.hellogit;
 
 public class HelloGit {
 	public static void main(String[] args) {
-		System.out.println("welcome to use git~");
+		System.out.println("welcome to use git");
 		System.out.println("you can upload your code on github~");
 		System.out.println("some update test");
 	}
